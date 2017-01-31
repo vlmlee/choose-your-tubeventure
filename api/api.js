8a3143f841945622af9d4f4a3ee4ef85f7a4f229
Michael Lee <mlee@vellichorlabs.com>
2017-01-31 14:51:34 -0900

    Added CORS for api server.

--- api/api.js
+++ api/api.js
@@ -14,6 +14,12 @@ app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:htt
 // Serve static assets
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/video/:id', (req, res) => {
 	MongoClient.connect(url, (err, db) => {
 		if (err) {
